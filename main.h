1c9dc6eb9421f439a5e06124cff2df1cdb45fc79
EmilMajid <emilinpoctu@gmail.com>
2025-04-03 06:51:58 +0000

    main.h-2

--- main.h
+++ main.h
@@ -1,15 +1,9 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef HEADER
+#define HEADER
 
-#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
-
+int _putchar(char c);
+int print_string(char *ptr);
+int print_number(int num);
 int _printf(const char *format, ...);
-void write_char(int *len, char c);
-void write_str(int *len, char *str);
-void write_int(int *len, int num);
 
 #endif
