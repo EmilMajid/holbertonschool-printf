7e056430e3bf16da61e6f6eaac9bfc556ab8b391
BashiriWinner <abeshiri.ab@gmail.com>
2025-04-04 16:35:08 +0400

    libraries added to main.h

--- main.h
+++ main.h
@@ -1,9 +1,16 @@
 #ifndef HEADER
 #define HEADER
 
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int print_string(char *ptr);
 int print_number(int num);
 int _printf(const char *format, ...);
+int _printf_char(unsigned int *a, char b);
 
 #endif
